e369be5206e4a6bce275610bdd586de261604b2d
Damian <skrzyniarz.damian@gmail.com>
2023-05-30 16:14:55 +0200

    [fix] add missing annotation to MessageRepository

---
+++ src/main/java/pl/dskrzyniarz/forum/controller/MessageController.java
@@ -0,0 +1,2 @@
+package pl.dskrzyniarz.forum.controller;public class MessageController {
+}
--- src/main/java/pl/dskrzyniarz/forum/repository/MessageRepository.java
+++ src/main/java/pl/dskrzyniarz/forum/repository/MessageRepository.java
@@ -1,7 +1,9 @@
 package pl.dskrzyniarz.forum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.dskrzyniarz.forum.entity.Message;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 }
