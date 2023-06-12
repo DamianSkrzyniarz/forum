e1eaaeac958461514530d964007b6481d16d562d
Damian <skrzyniarz.damian@gmail.com>
2023-06-12 15:39:34 +0200

    add topic repository

---
+++ src/main/java/pl/dskrzyniarz/forum/repository/TopicRepository.java
@@ -0,0 +1,9 @@
+package pl.dskrzyniarz.forum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.dskrzyniarz.forum.entity.Topic;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Integer> {
+}
