71abdc8fac2b741ca671374f51d38fe8934cfd29
Damian <skrzyniarz.damian@gmail.com>
2023-07-27 16:51:22 +0200

    add MessageService

---
+++ src/main/java/pl/dskrzyniarz/forum/service/MessageService.java
@@ -0,0 +1,42 @@
+package pl.dskrzyniarz.forum.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.dskrzyniarz.forum.entity.Message;
+import pl.dskrzyniarz.forum.repository.MessageRepository;
+
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+
+    public Message getMessage(int id){
+        return messageRepository.findById(id).get();
+    }
+
+    public Message saveMessage(Message message){
+        return messageRepository.save(message);
+    }
+
+    public void deleteMessage(int id){
+        Message message = messageRepository.findById(id).get();
+        messageRepository.delete(message);
+    }
+
+    public Message editMessage(Message newMessage, int id){
+        Message oldMessage = messageRepository.findById(id).get();
+        oldMessage.setBody(newMessage.getBody());
+        return messageRepository.save(oldMessage);
+    }
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
+
+    public List<Message> searchMessages(String searchedPhrase){
+        return messageRepository.findByBodyContaining(searchedPhrase);
+    }
+}
