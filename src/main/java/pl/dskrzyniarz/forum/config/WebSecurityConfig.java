01dc9fbba135a06ecee07622ababc54d1ca7814d
Damian <skrzyniarz.damian@gmail.com>
2023-08-10 14:28:51 +0200

    add user registration

--- src/main/java/pl/dskrzyniarz/forum/config/WebSecurityConfig.java
+++ src/main/java/pl/dskrzyniarz/forum/config/WebSecurityConfig.java
@@ -1,19 +1,25 @@
-package pl.dskrzyniarz.forum;
+package pl.dskrzyniarz.forum.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import pl.dskrzyniarz.forum.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/","/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
@@ -24,5 +30,11 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth, UserService userService) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
 
 }
